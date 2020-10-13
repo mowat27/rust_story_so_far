fafa0e881b53ae8f828a068ce7b9657bda6de50d
Adrian Mowat <adrian.mowat@gmail.com>
2020-10-13 15:03:10 +0100

    Added idiomatic iteration

--- rsmem/src/main.rs
+++ rsmem/src/main.rs
@@ -9,3 +9,31 @@ fn main() {
         i += 1;
     }
 }
+
+/* Handle an error using a match */
+
+// fn main() {
+//     let a = vec![0, 1, 2];
+
+//     let mut i = 0;
+//     while i < 5 {
+//         match a.get(i) {
+//             Some(val) => {
+//                 println!("a[{}] = {}", i, val);
+//             }
+//             None => {
+//                 println!("{} is out of bounds", i);
+//             }
+//         }
+//         i += 1;
+//     }
+// }
+
+/* More Idiomatic code */
+
+// fn main() {
+//     let a = vec![0, 1, 2];
+//     for (i, val) in a.iter().enumerate() {
+//         println!("a[{}] = {}", i, val)
+//     }
+// }
